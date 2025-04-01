5b680dd9831d15d8a0617d1a737c5260c58d3dd8
developerbola <developerbola08@gmail.com>
2025-04-01 14:42:20 +0500

    #10 Remove Tray

--- electron/main.js
+++ electron/main.js
@@ -1,11 +1,4 @@
-import {
-  app,
-  BrowserWindow,
-  globalShortcut,
-  ipcMain,
-  Tray,
-  Menu,
-} from "electron";
+import { app, BrowserWindow } from "electron";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
@@ -13,8 +6,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 let mainWindow;
-let popupWindow;
-let tray;
 
 // Create the main window
 function createWindow() {
@@ -44,68 +35,12 @@ function createWindow() {
   });
 }
 
-// Create the popup window
-function createPopupWindow() {
-  // If the popup window already exists, show it
-  if (popupWindow) {
-    popupWindow.show();
-    return;
-  }
-
-  popupWindow = new BrowserWindow({
-    width: 300,
-    height: 400,
-    frame: false,
-    alwaysOnTop: true,
-    resizable: false,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
-
-  // Load a separate HTML file for the popup
-  const popupURL = app.isPackaged
-    ? `file://${path.join(__dirname, "../dist/popup.html")}`
-    : "http://localhost:3005/popup.html"; 
-
-  popupWindow.loadURL(popupURL);
-
-  // Position the window near the tray (you can adjust this)
-  const trayBounds = tray.getBounds();
-  popupWindow.setPosition(trayBounds.x, trayBounds.y + trayBounds.height + 5);
-
-  // Hide the window when it loses focus
-  popupWindow.on("blur", () => {
-    popupWindow.hide();
-  });
-
-  popupWindow.on("closed", () => {
-    popupWindow = null;
-  });
-}
-
-// Create the system tray
-function createTray() {
-  tray = new Tray(path.join(__dirname, "../public/tray_icon.png"));
-  tray.on("click", () => {
-    createPopupWindow();
-  });
-  tray.setToolTip("Hashnote");
-}
-
 app.whenReady().then(() => {
   createWindow();
-  createTray(); // Initialize the tray
 
   if (process.platform === "darwin") {
     app.dock.setIcon(path.join(__dirname, "public", "icon.icns"));
   }
-
-  // Global Shortcut
-  globalShortcut.register("Control+Space", () => {
-    if (mainWindow) mainWindow.show();
-  });
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
