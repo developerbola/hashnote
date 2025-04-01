a3eaf36d28b494618a676548ff51368c958848a2
developerbola <developerbola08@gmail.com>
2025-04-01 16:47:09 +0500

    #11 Improve checkbox UI

--- src/components/ShowCase.jsx
+++ src/components/ShowCase.jsx
@@ -11,7 +11,7 @@ const ShowCase = ({ data, setActiveFile, loadFilesFromDisk }) => {
     const timeout = setTimeout(() => {
       setDatas(data);
       setOpacity(1);
-    }, 350);
+    }, 200);
     return () => clearTimeout(timeout);
   }, [data]);
 
@@ -79,7 +79,7 @@ const ShowCase = ({ data, setActiveFile, loadFilesFromDisk }) => {
     <div
       ref={showcaseRef}
       style={{
-        transition: "opacity 200ms ease-in-out",
+        transition: "opacity 100ms ease-in-out",
         opacity: opacity,
       }}
     >
